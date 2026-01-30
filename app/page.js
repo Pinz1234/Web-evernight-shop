e2975a403c8af71847df86aa24cd4b1aba0c7f0a
Pinz1234 <luthfikeren545@gmail.com>
2026-01-31 02:30:04 +0700

    Add files via upload

--- app/page.js
+++ app/page.js
@@ -1,31 +1,9 @@
-"use client";
+import dynamic from "next/dynamic";
 
-import LoadingScreen from "@/components/LoadingScreen";
-import Navbar from "@/components/Navbar";
-import Hero from "@/components/Hero";
-import PanelDO from "@/components/PanelDO";
-import PanelR18 from "@/components/PanelR18";
-import PanelR32 from "@/components/PanelR32";
-import ScriptBot from "@/components/ScriptBot";
-import SewaBot from "@/components/SewaBot";
-import Community from "@/components/Community";
-import Contact from "@/components/Contact";
-import Footer from "@/components/Footer";
+const ClientPage = dynamic(() => import("./ClientPage"), {
+  ssr: false,
+});
 
-export default function Home() {
-  return (
-    <>
-      <LoadingScreen />
-      <Navbar />
-      <Hero />
-      <PanelDO />
-      <PanelR18 />
-      <PanelR32 />
-      <ScriptBot />
-      <SewaBot />
-      <Community />
-      <Contact />
-      <Footer />
-    </>
-  );
+export default function Page() {
+  return <ClientPage />;
 }
