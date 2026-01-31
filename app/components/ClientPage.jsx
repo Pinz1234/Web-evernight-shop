ccfb1bd539a8950cf1bad40484d1dce769763715
Pinz1234 <luthfikeren545@gmail.com>
2026-01-31 09:28:32 +0700

    Add files via upload

---
+++ app/components/ClientPage.jsx
@@ -0,0 +1,31 @@
+"use client";
+
+import LoadingScreen from "./components/LoadingScreen";
+import Navbar from "./components/Navbar";
+import Hero from "./components/Hero";
+import PanelDO from "./components/PanelDO";
+import PanelR18 from "./components/PanelR18";
+import PanelR32 from "./components/PanelR32";
+import ScriptBot from "./components/ScriptBot";
+import SewaBot from "./components/SewaBot";
+import Community from "./components/Community";
+import Contact from "./components/Contact";
+import Footer from "./components/Footer";
+
+export default function ClientPage() {
+  return (
+    <>
+      <LoadingScreen />
+      <Navbar />
+      <Hero />
+      <PanelDO />
+      <PanelR18 />
+      <PanelR32 />
+      <ScriptBot />
+      <SewaBot />
+      <Community />
+      <Contact />
+      <Footer />
+    </>
+  );
+}
